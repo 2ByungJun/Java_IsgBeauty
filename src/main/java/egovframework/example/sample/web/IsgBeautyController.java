a7203f4fdad42145af4f21a707619bc6005b771d
sume1004 <lsuh0429@naver.com>
2020-07-15 15:11:56 +0900

    .

--- src/main/java/egovframework/example/sample/web/IsgBeautyController.java
+++ src/main/java/egovframework/example/sample/web/IsgBeautyController.java
@@ -17,6 +17,22 @@ package egovframework.example.sample.web;
 
 import java.util.List;
 
+import javax.annotation.Resource;
+import javax.json.JsonObject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springmodules.validation.commons.DefaultBeanValidator;
+
 import egovframework.example.sample.service.EmpService;
 import egovframework.example.sample.service.EmpVO;
 import egovframework.example.sample.service.MberService;
@@ -24,17 +40,6 @@ import egovframework.example.sample.service.MberVO;
 import egovframework.example.sample.service.SampleDefaultVO;
 import egovframework.rte.fdl.property.EgovPropertyService;
 import egovframework.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
-
-import javax.annotation.Resource;
-import javax.servlet.http.HttpSession;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.support.SessionStatus;
-import org.springframework.web.servlet.ModelAndView;
-import org.springmodules.validation.commons.DefaultBeanValidator;
 
 /**
  * @Class Name : EgovSampleController.java
@@ -72,7 +77,7 @@ public class IsgBeautyController {
 
 	/**
 	 * 로그인 페이지
-	 * 
+	 *
 	 * @param mberVO
 	 * @param model
 	 * @return
@@ -84,40 +89,40 @@ public class IsgBeautyController {
 
 		return "sample/login";
 	}
-	
+
 /*	@RequestMapping(value= "/loginCheck.do")
 	public ModelAndView loginCheck(@ModelAttribute MberVO vo, HttpSession session) {
-		
+
 		boolean result = empService.loginCheck(vo, session);
 		ModelAndView mav = new ModelAndView();
-		
+
 		mav.setViewName("login");
-		
+
 		if(result) {
 			mav.addObject("msg", "성공");
 		}else {
 			mav.addObject("msg", "실패");
 		}
-		
+
 		return mav;
 	}
-	
+
 	@RequestMapping(value= "/logout.do")
 	public ModelAndView loginout(HttpSession session) {
-		
+
 		empService.logout(session);
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("login");
 		mav.addObject("msg", "logout");
-		
+
 		return mav;
 	}*/
-	
-	
+
+
 
 	/**
 	 * 로그인-직원 등록 페이지
-	 * 
+	 *
 	 * @param searchVO
 	 * @param model
 	 * @return
@@ -132,7 +137,7 @@ public class IsgBeautyController {
 
 	/**
 	 * 고객 리스트
-	 * 
+	 *
 	 * @param mberVO
 	 * @param model
 	 * @return
@@ -142,11 +147,55 @@ public class IsgBeautyController {
 	public String mberList(@ModelAttribute("searchVO") SampleDefaultVO searchVO, ModelMap model) throws Exception {
 		System.out.println("[고객 리스트]");
 
-		/** EgovPropertyService.sample */
+		return "sample/mberList";
+	}
+
+
+  @ResponseBody
+	@RequestMapping(value = "/mberList.json")
+	public String mberListJson(@ModelAttribute("searchVO") SampleDefaultVO searchVO,  ModelMap model, HttpServletRequest request, HttpServletResponse response) throws Exception  {
+	  System.out.println("sfc/reagent/selectReagentList.json----------");
+
+/*		searchVO.setPageUnit(propertiesService.getInt("pageUnit"));
+		searchVO.setPageSize(propertiesService.getInt("pageSize"));
+
+		PaginationInfo paginationInfo = new PaginationInfo();
+		paginationInfo.setCurrentPageNo(searchVO.getPageIndex());
+		paginationInfo.setRecordCountPerPage(searchVO.getPageUnit());
+		paginationInfo.setPageSize(searchVO.getPageSize());
+
+		searchVO.setFirstIndex(paginationInfo.getFirstRecordIndex());
+		searchVO.setLastIndex(paginationInfo.getLastRecordIndex());
+		searchVO.setRecordCountPerPage(paginationInfo.getRecordCountPerPage());*/
+
+		List<?> mberList = mberService.selectMberList(searchVO);
+		model.addAttribute("mberList", mberList);
+
+		int totCnt = mberService.selectMberListTotCnt(searchVO);
+		/*paginationInfo.setTotalRecordCount(totCnt);
+		model.addAttribute("paginationInfo", paginationInfo);
+*/
+	   JSONObject jsonString = new JSONObject();
+
+       jsonString.put("dataCnt", totCnt);
+       jsonString.put("data", mberList);
+
+		response.setContentType("text/json; charset=UTF-8");
+		response.getOutputStream().write(jsonString.toString().getBytes("UTF-8"));
+
+		return "";
+
+	}
+
+/*	@RequestMapping(value = "/mberList.json")
+	public String mberListJson(@ModelAttribute("searchVO") SampleDefaultVO searchVO, ModelMap model) throws Exception {
+		System.out.println("[고객 리스트]");
+
+		*//** EgovPropertyService.sample *//*
 		searchVO.setPageUnit(propertiesService.getInt("pageUnit"));
 		searchVO.setPageSize(propertiesService.getInt("pageSize"));
 
-		/** pageing setting */
+		*//** pageing setting *//*
 		PaginationInfo paginationInfo = new PaginationInfo();
 		paginationInfo.setCurrentPageNo(searchVO.getPageIndex());
 		paginationInfo.setRecordCountPerPage(searchVO.getPageUnit());
@@ -164,11 +213,11 @@ public class IsgBeautyController {
 		model.addAttribute("paginationInfo", paginationInfo);
 
 		return "sample/mberList";
-	}
+	}*/
 
 	/**
 	 * 직원 리스트
-	 * 
+	 *
 	 * @param searchVO
 	 * @param model
 	 * @return
@@ -198,14 +247,14 @@ public class IsgBeautyController {
 		int totCnt = empService.selectEmpListTotCnt(searchVO);
 		paginationInfo.setTotalRecordCount(totCnt);
 		model.addAttribute("paginationInfo", paginationInfo);
-		
+
 		System.out.println("_----------" + totCnt);
 		return "sample/empList";
 	}
 
 	/**
 	 * 고객 등록 페이지
-	 * 
+	 *
 	 * @param searchVO
 	 * @param model
 	 * @return
@@ -214,16 +263,16 @@ public class IsgBeautyController {
 	@RequestMapping(value = "/mberRegister.do")
 	public String mberRegister(@ModelAttribute("searchVO") EmpVO searchVO, ModelMap model) throws Exception {
 		System.out.println("[고객 등록 페이지]");
-		
+
 		List<?> listEmpNM = empService.selectListEmpNM(searchVO);
 		model.addAttribute("listEmpNM", listEmpNM);
-		
+
 		return "sample/mberRegister";
 	}
 
 	/**
 	 * 고객 등록 기능
-	 * 
+	 *
 	 * @param searchVO
 	 * @param sampleVO
 	 * @return
@@ -232,11 +281,11 @@ public class IsgBeautyController {
 	@RequestMapping(value = "/addMber.do", method = RequestMethod.POST)
 	public String addMber(@ModelAttribute("searchVO") MberVO searchVO, MberVO sampleVO) throws Exception {
 		System.out.println("[고객 등록]");
-		
+
 		mberService.insertMber(sampleVO);
 		return "forward:/mberList.do";
 	}
-	
+
 	/**
 	 * 고객 상세화면 페이지
 	 * @param mberSn
@@ -249,14 +298,14 @@ public class IsgBeautyController {
 	public String mberView(@RequestParam("selectedId") String mberSn,
 			@ModelAttribute("searchVO") SampleDefaultVO searchVO, Model model) throws Exception {
 		System.out.println("[고객 상세화면]");
-		
+
 		MberVO sampleVO = new MberVO();
 		sampleVO.setMberSn(mberSn);
 		// 변수명은 CoC 에 따라 sampleVO
 		model.addAttribute("result", selectMber(sampleVO, searchVO));
 		return "sample/mberView";
 	}
-	
+
 	/**
 	 * 고객 수정화면 페이지
 	 * @param mberSn
@@ -269,14 +318,14 @@ public class IsgBeautyController {
 	public String mberEdit(@RequestParam("selectedId") String mberSn,
 			@ModelAttribute("searchVO") SampleDefaultVO searchVO, Model model) throws Exception {
 		System.out.println("[고객 수정화면]");
-		
+
 		MberVO sampleVO = new MberVO();
 		sampleVO.setMberSn(mberSn);
 		// 변수명은 CoC 에 따라 sampleVO
 		model.addAttribute("result", selectMber(sampleVO, searchVO));
 		return "sample/mberEdit";
 	}
-	
+
 	/**
 	 * 고객 조회 기능
 	 * @param sampleVO
@@ -288,8 +337,8 @@ public class IsgBeautyController {
 		System.out.println("[고객 조회 기능]");
 		return mberService.selectMber(sampleVO);
 	}
-	
-	
+
+
 	/**
 	 * 고객 삭제 기능
 	 * @return "forward:/mberList.do"
@@ -298,30 +347,30 @@ public class IsgBeautyController {
 	@RequestMapping("/deleteMber.do")
 	public String deleteMber(@RequestParam("selectedId") String mberSn) throws Exception {
 		System.out.println("[고객 삭제 기능]");
-		
+
 		MberVO sampleVO = new MberVO();
 		sampleVO.setMberSn(mberSn);
 		mberService.deleteMber(sampleVO);
 		return "forward:/mberList.do";
 	}
-	
+
 	/**
 	 * 고객 수정 기능
 	 * @return "forward:/mberList.do"
 	 * @exception Exception
 	 */
-	
+
 	@RequestMapping(value = "/updateMber.do", method = RequestMethod.POST)
 	public String updateMber(@ModelAttribute("mberVO") MberVO sampleVO) throws Exception {
 		System.out.println("[고객 수정 기능]");
-		
+
 		mberService.updateMber(sampleVO);
 		return "forward:/mberList.do";
 	}
 
 	/**
 	 * 직원 등록 페이지
-	 * 
+	 *
 	 * @param searchVO
 	 * @param model
 	 * @return
@@ -336,7 +385,7 @@ public class IsgBeautyController {
 
 	/**
 	 * 직원 등록 기능
-	 * 
+	 *
 	 * @param searchVO
 	 * @param sampleVO
 	 * @return
@@ -348,7 +397,7 @@ public class IsgBeautyController {
 		empService.insertEmp(sampleVO);
 		return "forward:/empList.do";
 	}
-	
+
 	/**
 	 * 직원 상세화면 페이지
 	 * @param empId
@@ -363,12 +412,12 @@ public class IsgBeautyController {
 		System.out.println("[직원 상세화면 페이지]");
 		EmpVO sampleVO = new EmpVO();
 		sampleVO.setEmpId(empId);
-		
+
 		// 변수명은 CoC 에 따라 sampleVO
 		model.addAttribute("result", selectEmp(sampleVO, searchVO));
 		return "sample/empView";
 	}
-	
+
 
 	/**
 	 * 직원 조회 기능
@@ -381,7 +430,7 @@ public class IsgBeautyController {
 		System.out.println("[직원 조회 기능]");
 		return empService.selectEmp(sampleVO);
 	}
-	
+
 	/**
 	 * 글 삭제기능
 	 * @param empId
@@ -391,13 +440,13 @@ public class IsgBeautyController {
 	@RequestMapping("/deleteEmp.do")
 	public String deleteEmp(@RequestParam("selectedId") String empId) throws Exception {
 		System.out.println("[고객 삭제 기능]");
-		
+
 		EmpVO sampleVO = new EmpVO();
 		sampleVO.setEmpId(empId);
 		empService.deleteEmp(sampleVO);
 		return "forward:/empList.do";
 	}
-	
+
 	/**
 	 * 고객 수정화면 페이지
 	 * @param empId
@@ -410,16 +459,16 @@ public class IsgBeautyController {
 	public String empEdit(@RequestParam("selectedId") String empId,
 			@ModelAttribute("searchVO") SampleDefaultVO searchVO, Model model) throws Exception {
 		System.out.println("[직원 수정화면]");
-		
+
 		EmpVO sampleVO = new EmpVO();
 		sampleVO.setEmpId(empId);
 		// 변수명은 CoC 에 따라 sampleVO
 		model.addAttribute("result", selectEmp(sampleVO, searchVO));
 		return "sample/empEdit";
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @param sampleVO
 	 * @return
 	 * @throws Exception
@@ -427,15 +476,15 @@ public class IsgBeautyController {
 	@RequestMapping(value = "/updateEmp.do", method = RequestMethod.POST)
 	public String updateEmp(@ModelAttribute("empVO") EmpVO sampleVO) throws Exception {
 		System.out.println("[직원 수정 기능]");
-		
+
 		empService.updateEmp(sampleVO);
 		return "forward:/empList.do";
 	}
-	
+
 
 	/**
 	 * 글 등록 화면을 조회한다.
-	 * 
+	 *
 	 * @param searchVO
 	 *            - 목록 조회조건 정보가 담긴 VO
 	 * @param model
@@ -447,10 +496,10 @@ public class IsgBeautyController {
 	 * String addSampleView(@ModelAttribute("searchVO") SampleDefaultVO searchVO,
 	 * Model model) throws Exception { model.addAttribute("sampleVO", new
 	 * SampleVO()); return "sample/egovSampleRegister"; }
-	 * 
+	 *
 	 *//**
 		 * 글을 등록한다.
-		 * 
+		 *
 		 * @param sampleVO
 		 *            - 등록할 정보가 담긴 VO
 		 * @param searchVO
@@ -464,18 +513,18 @@ public class IsgBeautyController {
 	 * String addSample(@ModelAttribute("searchVO") SampleDefaultVO searchVO,
 	 * SampleVO sampleVO, BindingResult bindingResult, Model model, SessionStatus
 	 * status) throws Exception {
-	 * 
+	 *
 	 * // Server-Side Validation beanValidator.validate(sampleVO, bindingResult);
-	 * 
+	 *
 	 * if (bindingResult.hasErrors()) { model.addAttribute("sampleVO", sampleVO);
 	 * return "sample/egovSampleRegister"; }
-	 * 
+	 *
 	 * sampleService.insertSample(sampleVO); status.setComplete(); return
 	 * "forward:/egovSampleList.do"; }
-	 * 
+	 *
 	 *//**
 		 * 글 수정화면을 조회한다.
-		 * 
+		 *
 		 * @param id
 		 *            - 수정할 글 id
 		 * @param searchVO
@@ -491,10 +540,10 @@ public class IsgBeautyController {
 	 * Exception { SampleVO sampleVO = new SampleVO(); sampleVO.setId(id); // 변수명은
 	 * CoC 에 따라 sampleVO model.addAttribute(selectSample(sampleVO, searchVO));
 	 * return "sample/egovSampleRegister"; }
-	 * 
+	 *
 	 *//**
 		 * 글을 조회한다.
-		 * 
+		 *
 		 * @param sampleVO
 		 *            - 조회할 정보가 담긴 VO
 		 * @param searchVO
@@ -507,10 +556,10 @@ public class IsgBeautyController {
 	 * public SampleVO selectSample(SampleVO sampleVO, @ModelAttribute("searchVO")
 	 * SampleDefaultVO searchVO) throws Exception { return
 	 * sampleService.selectSample(sampleVO); }
-	 * 
+	 *
 	 *//**
 		 * 글을 수정한다.
-		 * 
+		 *
 		 * @param sampleVO
 		 *            - 수정할 정보가 담긴 VO
 		 * @param searchVO
@@ -524,18 +573,18 @@ public class IsgBeautyController {
 	 * updateSample(@ModelAttribute("searchVO") SampleDefaultVO searchVO, SampleVO
 	 * sampleVO, BindingResult bindingResult, Model model, SessionStatus status)
 	 * throws Exception {
-	 * 
+	 *
 	 * beanValidator.validate(sampleVO, bindingResult);
-	 * 
+	 *
 	 * if (bindingResult.hasErrors()) { model.addAttribute("sampleVO", sampleVO);
 	 * return "sample/egovSampleRegister"; }
-	 * 
+	 *
 	 * sampleService.updateSample(sampleVO); status.setComplete(); return
 	 * "forward:/egovSampleList.do"; }
-	 * 
+	 *
 	 *//**
 		 * 글을 삭제한다.
-		 * 
+		 *
 		 * @param sampleVO
 		 *            - 삭제할 정보가 담긴 VO
 		 * @param searchVO
